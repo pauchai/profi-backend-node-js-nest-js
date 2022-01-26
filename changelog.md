d94457f2cf5e900a331bfb62f89d304570c77d1f
Pauk <paul.khimyack@gmail.com>
2022-01-26 13:48:40 +0200

    install sequelize postgres. setup users module

--- changelog.md
+++ changelog.md
@@ -4,3 +4,10 @@ nest new nest-course
 npm run start:dev
 
 simple controller
+
+npm install --save @nestjs/sequelize sequelize sequelize-typescript
+npm install --save pg pg-hstore
+npm install --save-dev @types/sequelize
+nest generate module users
+nest generate controller users
+nest generate service users
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import {SequelizeModule} from "@nestjs/sequelize"
+import { UsersModule } from './users/users.module';
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService]
+    controllers: [],
+    providers: [],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'postgres',
+            database: 'nest-course',
+            models: [],
+            autoLoadModels: true
+
+        }),
+        UsersModule
+    ]
 })
 export class AppModule{}
